4fbeab179dd777af5acce181bc788ea891fabf7d
sajan Limbu <limbusajan087@gmail.com>
2024-01-02 23:34:31 +0545

    learned simple hooks

--- for_react/src/App.jsx
+++ for_react/src/App.jsx
@@ -1,11 +1,38 @@
-
+import React,  { useState } from 'react';
 
 
 
 function App() {
 
+
+  let [counter, setCounter] = useState(2)
+
+  const addValue = () => {
+    console.log("clicked", counter);
+    //counter = counter +  1
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+      console.log("removed", counter);
+      setCounter (counter - 1)
+  }
+
   return (
+    <>
     <h1>Hello world | Sajan Limbu</h1>
+    <h2>I am {counter}</h2>
+
+    <button onClick={addValue}
+    > Add value {counter}
+    </button>
+
+  <button onClick={removeValue}> remove value {counter}</button>
+    <p> footer: {counter} </p>
+
+
+    </>
+    
   )
 }
 
